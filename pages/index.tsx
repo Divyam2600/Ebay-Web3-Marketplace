55b2fd180bec3c884d28f9bc550dbcc09c65a4b2
Divyam Agarwal <ag.divyam12@gmail.com>
2022-10-28 14:43:54 +0530

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { useActiveListings, useContract } from "@thirdweb-dev/react";
+import Head from "next/head";
+import { Fragment } from "react";
 import Header from "../components/Header";
 import Listing from "../components/Listing";
 import { ListingsLoader } from "../components/Loader";
@@ -12,20 +14,25 @@ const Home = () => {
   const { data: listings, isLoading: loadingListings } =
     useActiveListings(contract);
   return (
-    <div className="min-h-screen bg-gray-100/30">
-      <Header />
-      <main className="mx-auto max-w-6xl p-2 ">
-        {loadingListings ? (
-          <ListingsLoader />
-        ) : (
-          <div className="mx-auto grid gap-5 px-2 pb-2 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {listings?.map((listing, i) => (
-              <Listing listing={listing} key={i} />
-            ))}
-          </div>
-        )}
-      </main>
-    </div>
+    <Fragment>
+      <Head>
+        <title>Ebay Web3 Clone</title>
+      </Head>
+      <div className="min-h-screen bg-gray-100/30">
+        <Header />
+        <main className="mx-auto max-w-6xl p-2 ">
+          {loadingListings ? (
+            <ListingsLoader />
+          ) : (
+            <div className="mx-auto grid gap-5 px-2 pb-2 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {listings?.map((listing, i) => (
+                <Listing listing={listing} key={i} />
+              ))}
+            </div>
+          )}
+        </main>
+      </div>
+    </Fragment>
   );
 };
 
